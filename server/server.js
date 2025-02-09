6d3b91ce37218dcf13e4fe3a6163c44dc58ea5c2
Akarsh1412 <akarshkumar1412@gmail.com>
2025-02-09 19:15:31 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/database.js"
+import userRoute from "./routes/userRoute.js"
+import messageRoute from "./routes/messageRoute.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import { app, io, server } from "./socket/socket.js"
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+const corsOption = {
+    origin: "http://localhost:3000",
+    credentials: true,
+}
+
+app.use(cors(corsOption));
+
+//Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
+
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server Live at port ${PORT}`);
+});
